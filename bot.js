a78d82bd89ec704aebfed71138cdf32e5401b8f4
socket77455 <yadav77455sachin@gmail.com>
2018-11-29 12:30:50 +0530

    Added rich presence for the bot

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
 
-client.on('ready', () => {
+bot.on('ready', () => {
     console.log('I am ready!');
+    bot.user.setGame('ultra-h.com | ?info');
 });
 
-client.on('message', message => {
+bot.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);
